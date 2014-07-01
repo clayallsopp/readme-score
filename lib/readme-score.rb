fc2b2737d6f097f6217538f06df16f057e34373e
Clay Allsopp <clay.allsopp@gmail.com>
2014-07-01 13:46:49 -0700

    remove equation references

--- lib/readme-score.rb
+++ lib/readme-score.rb
@@ -8,7 +8,6 @@ require 'json'
 require "readme-score/version"
 require "readme-score/util"
 require "readme-score/document"
-require "readme-score/equation"
 
 module ReadmeScore
   ROOT = File.expand_path(File.join(File.dirname(__FILE__), ".."))
