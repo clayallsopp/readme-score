3a09a329cb5865ae2abc293746cd2191a61c34bc
Clay Allsopp <clay.allsopp@gmail.com>
2014-07-14 15:42:43 -0700

    switch to middleman

---
+++ Rakefile
@@ -0,0 +1,9 @@
+begin
+  require 'bundler'
+  Bundler.require
+rescue LoadError
+end
+
+task :open do
+  `open http://0.0.0.0:4567`
+end
---
+++ config.rb
@@ -0,0 +1,13 @@
+set :css_dir, 'stylesheets'
+
+set :js_dir, 'javascripts'
+
+set :images_dir, 'images'
+
+configure :development do
+  activate :livereload
+end
+
+# Build-specific configuration
+configure :build do
+end
