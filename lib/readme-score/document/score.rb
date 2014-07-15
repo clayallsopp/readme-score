671c59008b0f6dc2a469f6d1d2d8c11fffca4554
Clay Allsopp <clay.allsopp@gmail.com>
2014-07-15 14:50:44 -0700

    add human descriptions for breakdown criteria

--- lib/readme-score/document/score.rb
+++ lib/readme-score/document/score.rb
@@ -5,35 +5,42 @@ module ReadmeScore
       SCORE_METRICS = [
         {
           metric: :number_of_code_blocks,
+          description: "Number of code blocks",
           value_per: 5,
           max: 40
         },
         {
           metric: :number_of_non_code_sections,
+          description: "Number of non-code sections",
           value_per: 5,
           max: 30
         },
         {
           metric: :has_lists?,
+          description: "Has any lists?",
           value: 10
         },
         {
           metric: :number_of_images,
+          description: "Number of images",
           value_per: 5,
           max: 15
         },
         {
           metric: :number_of_gifs,
+          description: "Number of GIFs",
           value_per: 5,
           max: 15
         },
         {
           metric: :cumulative_code_block_length,
+          description: "Amount of code",
           value_per: 0.0009475244447271192,
           max: 10
         },
         {
           metric_name: :low_code_block_penalty,
+          description: "Penalty for lack of code blocks",
           metric: :number_of_code_blocks,
           if_less_than: 3,
           value: -10
@@ -46,7 +53,7 @@ module ReadmeScore
         @metrics = metrics
       end
 
-      def score_breakdown
+      def score_breakdown(as_description = false)
         breakdown = {}
         SCORE_METRICS.each { |h|
           metric_option = OpenStruct.new(h)
@@ -62,11 +69,19 @@ module ReadmeScore
           else
             metric_score_value = metrics.send(metric_option.metric) ? metric_option.value : 0
           end
-          breakdown[metric_name] = metric_score_value
+          if as_description
+            breakdown[metric_option.description] = metric_score_value
+          else
+            breakdown[metric_name] = metric_score_value
+          end
         }
         breakdown
       end
       alias_method :breakdown, :score_breakdown
+
+      def human_breakdown
+        score_breakdown(true)
+      end
 
       def total_score
         score = 0
