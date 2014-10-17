0a1b32dd9e6c9aa05de90456d4a6ddca3fe6fd13
Clay Allsopp <callsopp@palantir.com>
2014-10-16 19:36:27 -0700

    update ocktokit

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,7 @@ require 'webmock/rspec'
 module GithubSpecHelpers
   def stub_github_readme(protocol, repo, user, readme_filename)
     stub_request(:get, "https://api.github.com/repos/#{repo}/#{user}/readme").
-         with(:headers => {'Accept'=>'application/vnd.github.html', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Octokit Ruby Gem 3.2.0'})
+         with(:headers => {'Accept'=>'application/vnd.github.html', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3'})
   end
 end
 
