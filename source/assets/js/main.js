e097ce713ba808b6e3ebdd082e2902cec590a8e9
Yassir Barchi <dev.yassir@gmail.com>
2018-01-17 04:48:44 +0100

    Fix the query string parser

--- source/assets/js/main.js
+++ source/assets/js/main.js
@@ -216,16 +216,18 @@ var QueryString = function () {
   var vars = query.split("&");
   for (var i=0;i<vars.length;i++) {
     var pair = vars[i].split("=");
+    var key = decodeURIComponent(pair[0]);
+    var val = decodeURIComponent(pair[1]);
       // If first entry with this name
-    if (typeof query_string[pair[0]] === "undefined") {
-      query_string[pair[0]] = pair[1];
+    if (typeof query_string[key] === "undefined") {
+      query_string[key] = val;
       // If second entry with this name
-    } else if (typeof query_string[pair[0]] === "string") {
-      var arr = [ query_string[pair[0]], pair[1] ];
-      query_string[pair[0]] = arr;
+    } else if (typeof query_string[key] === "string") {
+      var arr = [ query_string[key], val ];
+      query_string[key] = arr;
       // If third or later entry with this name
     } else {
-      query_string[pair[0]].push(pair[1]);
+      query_string[key].push(val);
     }
   } 
     return query_string;
